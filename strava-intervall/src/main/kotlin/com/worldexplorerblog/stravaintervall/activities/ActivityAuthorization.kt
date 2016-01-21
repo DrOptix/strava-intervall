e0c1705f4e975bfbf0730d3a87ef1e9a148bf4df
Cristian "Dr.Optix" Dinu <dr.optix@gmail.com>
2016-01-21 22:52:09 +0200

    WIP: Working on a new idea for the authorization process
    This commit is more for backup purposes. Something funky
    is happening with IDEA and/or gradle.
    
    The build fails like a complete idiot. When I want it to
    blast me with exception and crash info it is not
    happening, but when I try to have a nice life it bugs me
    like a mad man.

--- strava-intervall/src/main/kotlin/com/worldexplorerblog/stravaintervall/activities/ActivityAuthorization.kt
+++ strava-intervall/src/main/kotlin/com/worldexplorerblog/stravaintervall/activities/ActivityAuthorization.kt
@@ -3,73 +3,42 @@ package com.worldexplorerblog.stravaintervall.activities
 import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
+import android.view.Window
 import com.worldexplorerblog.stravaintervall.R
-import com.worldexplorerblog.stravaintervall.fragments.AuthorizationOAuthFragment
-import com.worldexplorerblog.stravaintervall.fragments.AuthorizationMessageFragment
+import javastrava.api.v3.auth.model.Token
+import javastrava.api.v3.service.Strava
 
 class ActivityAuthorization : FragmentActivity() {
+    var storedToken: String
+        get() {
+            return getSharedPreferences(this.getString(R.string.app_name), Context.MODE_PRIVATE)
+                    .getString(this.getString(R.string.token_name), "token")
+        }
+        set(value) {
+            getSharedPreferences(this.getString(R.string.app_name), Context.MODE_PRIVATE)
+                    .edit()
+                    .putString(this.getString(R.string.token_name), value)
+                    .commit()
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.setContentView(R.layout.authorization_activity)
 
-        when (this.intent.getStringExtra("show-authorization-welcome")) {
-            null, "yes".toUpperCase() -> {
-                val appName = this.getString(R.string.app_name)
-                val storedToken = this.getSharedPreferences(appName, Context.MODE_PRIVATE)
-                        .getString("token", "")
+        val stravaApi = this.createStravaApi()
 
-                when (isValidToken(storedToken)) {
-                    true -> {
-                        // TODO: load interval selection activity
-                    }
-
-                    false -> {
-                        initializeWelcomeFragment()
-                    }
-                }
-            }
-
-            "no".toUpperCase() -> {
-                initializeOAuthFragment()
-            }
+        try {
+            stravaApi.authenticatedAthlete
+        } catch(ex: RuntimeException) {
+            val message = ex.message
         }
     }
 
-    private fun onConnectWithStravaClick() {
-        initializeOAuthFragment()
-    }
-
-    private fun onOAuthWorkflowFinished(token: String) {
-        this.getSharedPreferences(this.getString(R.string.app_name), Context.MODE_PRIVATE)
-                .edit()
-                .putString("token", token)
-                .commit()
-    }
-
-    private fun initializeWelcomeFragment() {
-        val fragmentWelcome = AuthorizationMessageFragment()
-        this.supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, fragmentWelcome)
-                .commit()
-
-        fragmentWelcome.onConnectWithStravaClick = {
-            run { this.onConnectWithStravaClick() }
-        }
-    }
-
-    private fun initializeOAuthFragment() {
-        val fragmentOAuth = AuthorizationOAuthFragment()
-        this.supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, fragmentOAuth)
-                .commit()
-
-        fragmentOAuth.onOAuthWorkflowFinished = {
-            token ->
-            run { this.onOAuthWorkflowFinished(token) }
-        }
-    }
-
-    private fun isValidToken(token: String): Boolean {
-        return true
+    private fun createStravaApi(): Strava {
+        val token = Token()
+        token.token = this.storedToken
+        return Strava(token)
     }
 }
