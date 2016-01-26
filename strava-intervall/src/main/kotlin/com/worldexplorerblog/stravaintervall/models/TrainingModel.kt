665495d7ae9179a53e6781c8eff960155813a222
Cristian "Dr.Optix" Dinu <dr.optix@gmail.com>
2016-01-26 19:58:05 +0200

    Renamed the classes for shorter names

---
+++ strava-intervall/src/main/kotlin/com/worldexplorerblog/stravaintervall/models/TrainingModel.kt
@@ -0,0 +1,7 @@
+package com.worldexplorerblog.stravaintervall.models
+
+import java.util.*
+
+data class TrainingModel(var name: String,
+                         var intervals: ArrayList<TrainingIntervalModel>)
+
--- strava-intervall/src/main/kotlin/com/worldexplorerblog/stravaintervall/models/TrainingProgramModel.kt
+++
@@ -1,7 +0,0 @@
-package com.worldexplorerblog.stravaintervall.models
-
-import java.util.*
-
-data class TrainingProgramModel(var name: String,
-                                var intervals: ArrayList<TrainingIntervalModel>)
-
