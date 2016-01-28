361d634c97eb0d3c4e8758617c9446e2c7826363
Cristian "Dr.Optix" Dinu <dr.optix@gmail.com>
2016-01-28 15:29:25 +0200

    WIP: training execution activity

--- strava-intervall/src/main/kotlin/com/worldexplorerblog/stravaintervall/activities/TrainingExecutionActivity.kt
+++ strava-intervall/src/main/kotlin/com/worldexplorerblog/stravaintervall/activities/TrainingExecutionActivity.kt
@@ -2,18 +2,32 @@ package com.worldexplorerblog.stravaintervall.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.gson.Gson
 import com.worldexplorerblog.stravaintervall.R
 import com.worldexplorerblog.stravaintervall.fragments.TrainingExecutionDetailsFragment
+import com.worldexplorerblog.stravaintervall.models.TrainingIntervalModel
+import com.worldexplorerblog.stravaintervall.models.TrainingPlanModel
+import org.jetbrains.anko.toast
 
 class TrainingExecutionActivity : AppCompatActivity() {
+    private val trainingPlan by lazy {
+        Gson().fromJson(intent.getStringExtra("training-interval"), TrainingPlanModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.training_execution_activity)
 
         val trainingExecutionDetailsFragment = TrainingExecutionDetailsFragment()
+        trainingExecutionDetailsFragment.trainingPlan = trainingPlan
+        trainingExecutionDetailsFragment.onIntervalSelected = { trainingInterval -> onIntervalSelected(trainingInterval) }
         supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, trainingExecutionDetailsFragment)
                 .commit()
+    }
+
+    private fun onIntervalSelected(trainingInterval: TrainingIntervalModel) {
+        toast(trainingInterval.intensity.toString())
     }
 }
 
